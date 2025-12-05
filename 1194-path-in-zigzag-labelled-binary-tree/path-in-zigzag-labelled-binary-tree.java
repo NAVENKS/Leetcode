3dacdee2a57dc69e2e36f1da28df9f8fd924be9c
NAVEN K S <ksnaven@gmail.com>
2025-12-05 17:02:30 +0530

    Time: 12 ms (4.64%) | Memory: 48.7 MB (5.30%) - LeetSync

--- 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.java
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.java
@@ -1,26 +1,34 @@
+import java.util.*;
 class Solution {
-    public List<Integer> pathInZigZagTree(int label) {
-        List<Integer> res= new ArrayList<Integer>();
-        res.add(label);
-        while(label>1)
-        {
-            int pow=1;
-            while(pow<=label)
-                pow=pow*2;
-            pow=pow/2;
-            System.out.println("Pow:"+pow);
-            int nth=label-pow+1;
-            int prev_nth=(nth+1)/2;
-            System.out.println("nth:"+nth);
-            System.out.println("prev_nth:"+prev_nth);
-            label=pow-prev_nth;
-            System.out.println("label:"+label);
-            res.add(label);
-            System.out.println("New");
+    public List<Integer> pathInZigZagTree(int l) {
+        List<Integer> list = new ArrayList<>();
+        int sl = 1;
+        int p = 2;
+        while (l >= p) {
+            sl++;
+            p *= 2;
         }
-        List<Integer> rev= new ArrayList<Integer>();
-        for(int ind=res.size()-1; ind>=0;ind--)
-            rev.add(res.get(ind));
-        return rev;
+        System.out.print("sl " + sl);
+
+        while (sl != 0) {
+            int val;
+            int start = (int) Math.pow(2, sl - 1);
+            int end = (start * 2) - 1;
+            System.out.println(start + " " + end);
+
+            if (sl % 2 == 0) {
+                val = end - (l - start);
+                System.out.println("val" + val);
+                list.add(l);        
+                l = val / 2;        
+            } else {
+                list.add(l); 
+                l = (start + end - l) / 2;
+            }
+            sl--;
+        }
+
+        Collections.reverse(list);
+        return list;
     }
 }
