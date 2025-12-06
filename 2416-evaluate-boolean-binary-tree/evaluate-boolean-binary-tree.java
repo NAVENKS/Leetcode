aa9192e118091ff31fbd58e3e24cdfcee328c356
NAVEN K S <ksnaven@gmail.com>
2025-12-06 14:57:02 +0530

    Time: 16 ms (0.68%) | Memory: 46.3 MB (28.14%) - LeetSync

---
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean fun(TreeNode t){
+        boolean l=true,r=true;
+        if(t.left!=null && t.right!=null){
+            l=fun(t.left);
+            r=fun(t.right);
+        }
+        else if(t.left==null && t.right==null){
+            if(t.val==1)return true;
+            else return false;
+        }
+        if(t.val == 2){
+            System.out.println(l||r);
+            return l||r;
+        }
+        else return l&&r;
+    }
+    public boolean evaluateTree(TreeNode root) {
+        return fun(root);
+    }
+}
