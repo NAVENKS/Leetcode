8c58a500ac0bb76a2f4d3e26648af9f683dbc469
NAVEN K S <ksnaven@gmail.com>
2025-12-10 16:02:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (22.18%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
@@ -14,13 +14,10 @@
  * }
  */
 class Solution {
-    int i=1;
     TreeNode fun(TreeNode t,int s,int e,int n[]){
         int m=(s+e)/2;
-        if(e<0 || s>e)return null;
-        if(m>=n.length)return null;
+        if(e<0 || s>e || m>=n.length)return null;
         t.val=n[m];
-        System.out.println(m);
         t.left=fun(new TreeNode(),s,m-1,n);
         t.right=fun(new TreeNode(),m+1,e,n);
         return t;
@@ -29,6 +26,5 @@ class Solution {
         TreeNode root=new TreeNode();
         TreeNode t=root;
         return fun(root,0,n.length,n);
-        
     }
 }
