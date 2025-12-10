fefc81b88a57f2ef414e94c5473abe14c719a490
NAVEN K S <ksnaven@gmail.com>
2025-12-10 12:51:50 +0530

    Time: 11 ms (7.50%) | Memory: 51.8 MB (59.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,10 +3,7 @@ class Solution {
         int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
         Stack<int []>stack=new Stack<>();
         int u=1;
-        HashMap<Integer,Integer>map=new HashMap<>();
         int m[][]=new int [g.length][g[0].length];
-        // for(int i=0;i<m.length;i++)
-        // Arrays.fill(m[i],0);
         for(int i=0;i<g.length;i++){
             for(int j=0;j<g[0].length;j++){
                 if(g[i][j]=='1'){
