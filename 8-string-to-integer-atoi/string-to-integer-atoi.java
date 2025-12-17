8ad87eaf321706873c71c3cf2f549b1edf4b9486
NAVEN K S <ksnaven@gmail.com>
2025-12-17 18:27:43 +0530

    Time: 5 ms (6.15%) | Memory: 43.9 MB (25.77%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,28 @@
+class Solution {
+    void fun(String s,long n[],int in){
+        if(in>=s.length() || (s.charAt(in)-48)<0 || (s.charAt(in)-48)>9)return;
+        if(n[0]>Integer.MAX_VALUE)return;
+        long num=s.charAt(in)-48;
+        n[0]=(n[0]*10)+num;
+        fun(s,n,in+1);
+    }
+    public int myAtoi(String s) {
+        s=s.trim();
+        if(s.length()==0)return 0;
+        long n[]={0};
+        int sign=1;
+        if(s.charAt(0)=='-'){
+            sign=-1;
+            fun(s.substring(1,s.length()),n,0);
+        }
+        else if(s.charAt(0)=='+'){
+            fun(s.substring(1,s.length()),n,0);
+        }
+        else
+        fun(s,n,0);
+        System.out.print(n[0]);
+        if(n[0]>Integer.MAX_VALUE)
+        return sign==-1?Integer.MIN_VALUE:Integer.MAX_VALUE;
+        return (int)(n[0]*sign);
+    }
+}
