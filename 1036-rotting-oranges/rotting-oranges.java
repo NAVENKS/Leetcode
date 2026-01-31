7b380d79ed5afcd73c21a7e1cbf09db0b8404e08
NAVEN K S <ksnaven@gmail.com>
2026-01-31 11:13:01 +0530

    Time: 10 ms (4.82%) | Memory: 45.1 MB (5.49%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -7,6 +7,8 @@ class Pair{
 }
 class Solution {
     public int orangesRotting(int[][] g) {
+        int drow[]={-1,0, 1, 0};
+        int dcol[]={ 0,1, 0,-1};
         int r=g.length,c=g[0].length;
         int vis[][]=new int [r][c];
         Queue<Pair>q=new LinkedList<>();
@@ -21,8 +23,6 @@ class Solution {
                 }
             }
         }
-        // if(g.length==1 && not==0)
-        // return -1;
         while(!q.isEmpty()){
             int s=q.size();
             count++;
@@ -30,26 +30,32 @@ class Solution {
                 Pair poll=q.poll();
                 int a=poll.a;
                 int b=poll.b;
-                if(a-1>=0 && g[a-1][b]==1 && vis[a-1][b]==0){
-                    q.offer(new Pair(a-1,b));
-                    vis[a-1][b]=1;
+                for(int j=0;j<4;j++)
+                {if(a+drow[j]>=0 && a+drow[j]<r && b+dcol[j]>=0 && b+dcol[j]<c && g[a+drow[j]][b+dcol[j]]==1 && vis[a+drow[j]][b+dcol[j]]==0){
+                    q.offer(new Pair(a+drow[j],b+dcol[j]));
+                    vis[a+drow[j]][b+dcol[j]]=1;
                     not--;
-                }
-                if(a+1<r && g[a+1][b]==1 && vis[a+1][b]==0){
-                    q.offer(new Pair(a+1,b));
-                    vis[a+1][b]=1;
-                    not--;
-                }
-                if(b-1>=0 && g[a][b-1]==1 && vis[a][b-1]==0){
-                    q.offer(new Pair(a,b-1));
-                    vis[a][b-1]=1;
-                    not--;
-                }
-                if(b+1<c && g[a][b+1]==1 && vis[a][b+1]==0){
-                    q.offer(new Pair(a,b+1));
-                    vis[a][b+1]=1;
-                    not--;
-                }
+                }}
+                // if(a-1>=0 && g[a-1][b]==1 && vis[a-1][b]==0){
+                //     q.offer(new Pair(a-1,b));
+                //     vis[a-1][b]=1;
+                //     not--;
+                // }
+                // if(a+1<r && g[a+1][b]==1 && vis[a+1][b]==0){
+                //     q.offer(new Pair(a+1,b));
+                //     vis[a+1][b]=1;
+                //     not--;
+                // }
+                // if(b-1>=0 && g[a][b-1]==1 && vis[a][b-1]==0){
+                //     q.offer(new Pair(a,b-1));
+                //     vis[a][b-1]=1;
+                //     not--;
+                // }
+                // if(b+1<c && g[a][b+1]==1 && vis[a][b+1]==0){
+                //     q.offer(new Pair(a,b+1));
+                //     vis[a][b+1]=1;
+                //     not--;
+                // }
             }
         }
         if(count==0 && not==0 && g.length==1)
