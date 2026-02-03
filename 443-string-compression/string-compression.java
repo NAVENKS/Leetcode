983a8d8a74bff754a4228eae5ad4953dc864c0aa
NAVEN K S <ksnaven@gmail.com>
2026-02-03 20:42:35 +0530

    Time: 19 ms (6.05%) | Memory: 46.4 MB (7.75%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int compress(char[] s) {
+        int c = 1;
+        char prev = s[0];
+        StringBuilder count = new StringBuilder();
+        int i = 0;
+        for (int j = 1; j < s.length; j++) {
+            if (prev == s[j]) {
+                c++;
+            } else {
+                System.out.println(prev);
+                if (c == 1) {
+                    s[i++] = prev;
+                    c = 1;
+                    prev = s[j];
+                } else {
+                    count.append(c + "");
+                    s[i++] = prev;
+                    for (char ch : count.toString().toCharArray()) {
+                        s[i++] = ch;
+                    }
+                    c = 1;
+                    prev = s[j];
+                    count = new StringBuilder();
+                }
+            }
+        }
+        if (c == 1) {
+            s[i++] = prev;
+        } else {
+            count.append(c + "");
+            s[i++] = prev;
+            for (char ch : count.toString().toCharArray()) {
+                s[i++] = ch;
+            }
+
+        }
+        return i;
+    }
+}
