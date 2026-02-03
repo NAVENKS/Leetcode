3ee046708b82eeb7eddd308e5b703069e896b5ac
NAVEN K S <ksnaven@gmail.com>
2026-02-03 21:01:20 +0530

    Time: 1 ms (45.91%) | Memory: 47 MB (45.52%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -2,13 +2,14 @@ class Solution {
     public int[] sortArrayByParity(int[] n) {
         int ans[]=new int[n.length];
         int j=0;
-        for(int i=0;i<n.length;i++){
-            if(n[i]%2!=0){
-                j=i;
-                break;
-            }
-        }
-        for(int i=j+1;i<n.length;i++){
+        // for(int i=0;i<n.length;i++){
+        //     if(n[i]%2!=0){
+        //         j=i;
+        //         break;
+        //     }
+        // }
+        for(int i=1;i<n.length;i++){
+            if(n[j]%2==0)j++;
             if(n[j]%2!=0 && n[i]%2==0){
                 int t=n[j];
                 n[j]=n[i];
