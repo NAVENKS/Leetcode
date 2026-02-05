3d2b878c1d5ae7fc83fad5f06422b11968c02ca9
NAVEN K S <ksnaven@gmail.com>
2026-02-05 16:02:52 +0530

    Time: 11 ms (5.98%) | Memory: 45.4 MB (8.68%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String reverseStr(String str, int k) {
+        StringBuilder ans = new StringBuilder(str);
+        int c=str.length()/(2*k);
+        System.out.println(c);
+        int s=(2*k);
+        // if(str.length()<k)return str;
+        for(int i=1;i<=c;i++){
+            System.out.print(s+" ");
+            int start=s-(2*k);
+            int end=start+k-1;
+            while(start<=end){
+                ans.setCharAt(start,str.charAt(end));
+                ans.setCharAt(end,str.charAt(start));
+                start++;
+                end--;
+            }
+            s+=2*k;
+            System.out.println(start+" "+end);
+        }
+        s=s-(2*k);
+        System.out.println(s+" After ");
+        if(s<(str.length())){
+            if((str.length()-s)>=k){
+                int start=s;
+                int end=s+k-1;
+                System.out.println(start+" "+end);
+                while(start<=end){
+                ans.setCharAt(start,str.charAt(end));
+                ans.setCharAt(end,str.charAt(start));
+                start++;
+                end--;
+            }
+            }
+            else{
+                int start=s;
+                int end=str.length()-1;
+                 System.out.println(start+" "+end);
+                while(start<=end){
+                ans.setCharAt(start,str.charAt(end));
+                ans.setCharAt(end,str.charAt(start));
+                start++;
+                end--;
+            }
+        }
+    }
+        return ans.toString();
+    }
+}
