76fb26be7131818c8a6f3105c81983fd48171096
NAVEN K S <ksnaven@gmail.com>
2026-02-06 11:07:14 +0530

    Time: 686 ms (15.58%) | Memory: 43.2 MB (44.45%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -3,11 +3,17 @@ class Solution {
         int ans=s.length();
         for(int i=0;i<s.length();i++){
             for(int j=i+1;j<s.length();j++){
-                String sub=s.substring(i,j+1);
-                StringBuilder rev=new StringBuilder(sub);
-                rev=rev.reverse();
-                // System.out.print(" "+rev+" "+sub);
-                if(sub.equals(rev.toString())){
+                boolean flag=true;
+                int l=i,h=j;
+                while(l<=h){
+                    if(s.charAt(l)!=s.charAt(h)){
+                        flag=false;
+                        break;
+                    }
+                    l++;
+                    h--;
+                }
+                if(flag){
                     ans++;
                 }
             }
