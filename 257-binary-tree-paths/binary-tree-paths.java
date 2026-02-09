272366cff80fa4eaa070414276ecd39309fe5e56
NAVEN K S <ksnaven@gmail.com>
2026-02-09 14:54:56 +0530

    Time: 4 ms (70.72%) | Memory: 49.3 MB (57.08%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -1,20 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> ans = new ArrayList<>();
-        if (root == null) return ans;
-        dfs(root, "", ans);
-        return ans;
-    }
-
-    private void dfs(TreeNode node, String path, List<String> ans) {
-        if (node == null) return;
-        path = path + node.val;
-        if (node.left == null && node.right == null) {
-            ans.add(path);
-            return;
+    List<String> ans = new ArrayList<>();
+    void fun(TreeNode t,String s){
+        if(t==null)return;
+        if(t.left==null && t.right==null){
+        s+=t.val+"";
+        ans.add(s);
         }
-        path += "->";
-        dfs(node.left, path, ans);
-        dfs(node.right, path, ans);
+        else
+        s+=t.val+"->";
+        fun(t.left,s);
+        fun(t.right,s);
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+        fun(root,"");
+        return ans;
     }
 }
