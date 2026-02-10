f10fa438611f4af4775c36bfe0bd53a2b937779d
NAVEN K S <ksnaven@gmail.com>
2026-02-10 11:42:25 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (27.83%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -14,51 +14,42 @@
  * }
  */
 class Solution {
-    int max=Integer.MIN_VALUE;
-    HashMap<Integer,List<Integer>>m=new HashMap<>();
-    HashMap<Integer,Integer>f=new HashMap<>();
-    void fun(TreeNode t){
-        if(t==null)return;
-        if(f.containsKey(t.val)){
-            int fre=f.get(t.val);
-            max=Math.max(max,fre+1);
-            int i=m.get(fre).indexOf(t.val);
-            m.get(fre).remove(i);
-            if(m.containsKey(fre+1)){
-                m.get(fre+1).add(t.val);
-            }
-            else{
-            m.put(fre+1,new ArrayList<>());
-            m.get(fre+1).add(t.val);
-            }
-            f.put(t.val,f.get(t.val)+1);
-        }
-        else{
-            max=Math.max(max,1);
-             if(m.containsKey(1)){
-                m.get(1).add(t.val);
-                f.put(t.val,1);
-            }
-            else{
-            m.put(1,new ArrayList<>());
-            m.get(1).add(t.val);
-             f.put(t.val,1);
-            }
-        }
-        fun(t.left);
-        fun(t.right);
-    }
+    int count = 0;
+    int maxCount = 0;
+    Integer prev = null;
+    List<Integer> result = new ArrayList<>();
+
     public int[] findMode(TreeNode root) {
-        fun(root);
-        System.out.print(m);
-        System.out.println(f);
-        System.out.print(max);
-        int n[]=new int[m.get(max).size()];
-        List<Integer>l=m.get(max);
-        for(int i=0;i<l.size();i++){
-            n[i]=l.get(i);
+        inorder(root);
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
         }
-        // return new int[]{0};
-        return n;
+        return ans;
+    }
+
+    void inorder(TreeNode node) {
+        if (node == null)
+            return;
+
+        inorder(node.left);
+
+        if (prev == null || node.val != prev) {
+            count = 1;
+        } else {
+            count++;
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            result.clear();
+            result.add(node.val);
+        } else if (count == maxCount) {
+            result.add(node.val);
+        }
+
+        prev = node.val;
+
+        inorder(node.right);
     }
 }
