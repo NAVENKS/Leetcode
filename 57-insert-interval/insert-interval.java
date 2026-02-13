90c502c71ae6487fbd03dc1cec7ee2a7855af25a
NAVEN K S <ksnaven@gmail.com>
2026-02-13 11:23:57 +0530

    Time: 1 ms (97.59%) | Memory: 47.2 MB (33.35%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,30 +1,32 @@
 class Solution {
     public int[][] insert(int[][] arr, int[] ne) {
-        int n[][]=new int[arr.length+1][2];
-        int k=0;
-        int in=0;
-        for(in =0 ;in<arr.length;in++)
-        n[in]=arr[in];
-        n[in]=ne;
-        Arrays.sort(n,(a,b)->a[0]-b[0]);
-        List<int []>l=new ArrayList<>();
-        int i=0;
-        while(i<n.length){
-            int j=i+1;
-            int z=n[i][1];
-            while(j<n.length && z>=n[j][0]){
-                z=Math.max(z,n[j][1]);
+        int[][] n = new int[arr.length + 1][2];
+        int i = 0, k = 0;
+        while (i < arr.length && arr[i][0] < ne[0]) {
+            n[k++] = arr[i++];
+        }
+        n[k++] = ne;
+        while (i < arr.length) {
+            n[k++] = arr[i++];
+        }
+        List<int[]> l = new ArrayList<>();
+        i = 0;
+        while (i < n.length) {
+            int j = i + 1;
+            int z = n[i][1];
+            while (j < n.length && z >= n[j][0]) {
+                z = Math.max(z, n[j][1]);
                 j++;
             }
-            l.add(new int[]{n[i][0],z});
-            if(i==j)
-            i++;
+            l.add(new int[] { n[i][0], z });
+            if (i == j)
+                i++;
             else
-            i=j;
+                i = j;
         }
-        int ans[][]=new int[l.size()][2];
-        for(int b=0;b<l.size();b++){
-            ans[b]=l.get(b);
+        int ans[][] = new int[l.size()][2];
+        for (int b = 0; b < l.size(); b++) {
+            ans[b] = l.get(b);
         }
         return ans;
     }
