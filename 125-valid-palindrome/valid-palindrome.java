de8f5b0c8e53b51047a35eb108d6b81547fccf07
NAVEN K S <ksnaven@gmail.com>
2026-02-19 20:54:49 +0530

    Time: 3 ms (62.65%) | Memory: 44.4 MB (67.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // s=s.replaceAll("\\s+","");
-        s=s.toLowerCase();
         int i=0,j=s.length()-1;
         while(i<j){
             while(i<s.length() && !(Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i))))i++;
             while(j>=0 && !(Character.isLetter(s.charAt(j)) || Character.isDigit(s.charAt(j))))j--;
-            if(i<s.length() && j>=0 && s.charAt(i)!=s.charAt(j)){
+            if(i<s.length() && j>=0 && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
                 return false;
             }
             i++;
