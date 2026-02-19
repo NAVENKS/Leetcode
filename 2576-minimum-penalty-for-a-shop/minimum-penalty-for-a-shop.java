7258f875558d28262f8d3277ea8252225b73323a
NAVEN K S <ksnaven@gmail.com>
2026-02-19 11:28:54 +0530

    Time: 96 ms (5.31%) | Memory: 55.4 MB (8.01%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int bestClosingTime(String str) {
+        int p[]=new int [str.length()+1];
+        int n[]=new int [str.length()+1];
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='N')
+            count++;
+            p[i+1]=count;
+        }
+        if(count==str.length())return 0;
+        count=0;
+        for(int i=str.length()-1;i>=0;i--){
+            n[i+1]=count;
+            if(str.charAt(i)=='Y')
+            count++;
+        }
+        n[0]=count;
+        System.out.println(Arrays.toString(p));
+        System.out.println(Arrays.toString(n));
+        int max=0;
+        for(int i=0;i<p.length;i++){
+            if((p[max]+n[max])>(p[i]+n[i]))
+            max=i;
+        }
+        return max;
+    }
+}
