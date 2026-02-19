ff9a11a5e05e8b4badb26fa25837fb29f086e004
NAVEN K S <ksnaven@gmail.com>
2026-02-19 14:09:38 +0530

    Time: 1 ms (99.85%) | Memory: 43.6 MB (72.78%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,40 +1,28 @@
 class Solution {
     public String addBinary(String a, String b) {
+
         StringBuilder ans = new StringBuilder();
-        int i=a.length()-1,j=b.length()-1;
-        int carry=0;
+
+        int i = a.length()-1;
+        int j = b.length()-1;
+
+        int carry = 0;
+
         while(i>=0 || j>=0 || carry!=0){
-            int sum=carry;
-            if(i>=0){
-                sum+=Integer.parseInt(a.charAt(i)+"");
-            }
-            if(j>=0){
-                sum+=Integer.parseInt(b.charAt(j)+"");
-            }
-            if(sum==3){
-                carry=1;
-                ans.append(1);
-            }
-            else if(sum==2){
-                carry=1;
-                ans.append(0);
-            }
-            else{
-                carry=0;
-                ans.append(sum);
-            }
-            i--;
-            j--;
+
+            int sum = carry;
+
+            if(i>=0)
+                sum += a.charAt(i--) - '0';
+
+            if(j>=0)
+                sum += b.charAt(j--) - '0';
+
+            ans.append(sum % 2);
+
+            carry = sum / 2;
         }
-        i=0;
-        j=ans.length()-1;
-        while(i<j){
-            char ch=ans.charAt(i);
-            ans.setCharAt(i,ans.charAt(j));
-            ans.setCharAt(j,ch);
-            j--;
-            i++;
-        }
-        return ans.toString();
+
+        return ans.reverse().toString();
     }
 }
