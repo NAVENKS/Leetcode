cc2abc756404242783e6c0a400182e8d599fafb2
NAVEN K S <ksnaven@gmail.com>
2026-02-19 20:52:25 +0530

    Time: 3 ms (62.65%) | Memory: 44.7 MB (50.72%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.replaceAll("\\s+","");
+        // s=s.replaceAll("\\s+","");
         s=s.toLowerCase();
         int i=0,j=s.length()-1;
         while(i<j){
             while(i<s.length() && !(Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i))))i++;
             while(j>=0 && !(Character.isLetter(s.charAt(j)) || Character.isDigit(s.charAt(j))))j--;
             if(i<s.length() && j>=0 && s.charAt(i)!=s.charAt(j)){
-                // System.out.print(s.charAt(i)+" "+s.charAt(j));
                 return false;
             }
             i++;
