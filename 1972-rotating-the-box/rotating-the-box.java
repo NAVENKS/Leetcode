6f78aa85a8c2c21288ce6087275b99b5ef971b7b
NAVEN K S <ksnaven@gmail.com>
2026-02-20 14:44:37 +0530

    Time: 61 ms (5.04%) | Memory: 129.8 MB (5.78%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,47 @@
+class Solution {
+    public char [][] reverse(char [][]arr){
+        int c=arr[0].length,r=arr.length;
+        for(int j=0;j<r;j++){
+        int s=0,e=c-1;
+        while(s<e){
+            char ch=arr[j][s];
+            arr[j][s]=arr[j][e];
+            arr[j][e]=ch;
+            s++;
+            e--;
+        }
+        }
+        return arr;
+    }
+    public char[][] rotateTheBox(char[][] b) {
+        int c=b[0].length,r=b.length;
+        char arr[][]=new char[c][r];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[j][i]=b[i][j];
+            }
+        }
+        c=arr[0].length;
+        r=arr.length;
+        arr=reverse(arr);
+        for(int i=0;i<r;i++)
+        System.out.println(Arrays.toString(arr[i]));
+        for(int j=c-1;j>=0;j--){
+            Queue<Integer>space=new LinkedList<>();
+            for(int i=r-1;i>=0;i--){
+                if(arr[i][j]=='*')
+                space.clear();
+                else if(arr[i][j]=='.')
+                space.offer(i);
+                else{
+                    if(!space.isEmpty()){
+                        arr[space.poll()][j]='#';
+                        arr[i][j]='.';
+                        space.offer(i);
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+}
