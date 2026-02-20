e419336b8156efa7580d5c7e6f47126ac5c371e8
NAVEN K S <ksnaven@gmail.com>
2026-02-20 13:52:35 +0530

    Time: 3 ms (9.40%) | Memory: 43 MB (69.47%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,37 +1,22 @@
 class Solution {
-    public int[] build(String pattern){
-        int len=0;
-        int i=1;
-        int lps[]=new int[pattern.length()];
-        while(i<pattern.length()){
-            if(pattern.charAt(i)==pattern.charAt(len)){
-                len++;
-                lps[i]=len;
-                i++;
-            }
-            else{
-                if(len!=0){
-                    len=lps[len-1];
-                }
-                else{
-                    lps[i]=0;
-                    i++;
-                }
-            }
-        }
-        return lps;
-    }
-    public int strStr(String h, String n) {
-        int lps[]=build(n);
-        int i=0,j=0;
-        while(i<h.length()){
-            if(h.charAt(i)==n.charAt(j)){
+
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        int n = haystack.length();
+        int m = needle.length();
+        int[] lps = buildLPS(needle);
+        int i = 0;
+        int j = 0;
+        while (i < n) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
                 i++;
                 j++;
             }
-            if(j==n.length())
-            return i-j;
-            else if (i < h.length() && h.charAt(i) != n.charAt(j)) {
+            if (j == m) {
+                return i - j;
+            }
+            else if (i < n && haystack.charAt(i) != needle.charAt(j)) {
+
                 if (j != 0) {
                     j = lps[j - 1];
                 } else {
@@ -39,6 +24,38 @@ class Solution {
                 }
             }
         }
+
         return -1;
+    }
+    private int[] buildLPS(String pattern) {
+
+        int m = pattern.length();
+        int[] lps = new int[m];
+
+        int len = 0;
+        int i = 1;
+
+        while (i < m) {
+
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+
+            else {
+
+                if (len != 0) {
+                    len = lps[len - 1];
+                }
+
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
     }
 }
