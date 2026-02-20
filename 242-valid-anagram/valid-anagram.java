ac898947396932962347a132946f8462914fd893
NAVEN K S <ksnaven@gmail.com>
2026-02-20 09:29:05 +0530

    Time: 2 ms (99.45%) | Memory: 45.1 MB (51.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        if(s.length()!=t.length())return false;
-        for(char ch:s.toCharArray())
-        map.put(ch,map.getOrDefault(ch,0)+1);
-        for(char ch:t.toCharArray()){
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)-1);
-                if(map.get(ch)<0)
+        if(s.length() != t.length()) return false;
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray())
+            freq[ch - 'a']++;
+        for(char ch : t.toCharArray()){
+            if(--freq[ch - 'a'] < 0)
                 return false;
-            }
-            else
-            return false;
         }
         return true;
     }
