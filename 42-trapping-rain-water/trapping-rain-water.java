fc29c6c69834f06fe9994363434b8233757f6b51
NAVEN K S <ksnaven@gmail.com>
2026-02-23 18:08:03 +0530

    Time: 15 ms (5.38%) | Memory: 48.5 MB (33.69%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        int max=0;
+        int pre[]=new int[height.length];
+        int suf[]=new int[height.length];
+        for(int i=0;i<height.length;i++){
+            if(max<=height[i])
+            max=height[i];
+            pre[i]=max;
+        }
+        max=0;
+        for(int i=height.length-1;i>=0;i--){
+            if(max<=height[i])
+            max=height[i];
+            suf[i]=max;
+        }
+        int water=0;
+        System.out.println(Arrays.toString(pre));
+        System.out.println(Arrays.toString(suf));
+        for(int i=0;i<height.length;i++){
+            int min=Math.min(pre[i],suf[i]);
+            water+=min-height[i];
+        }
+        return water;
+    }
+}   
