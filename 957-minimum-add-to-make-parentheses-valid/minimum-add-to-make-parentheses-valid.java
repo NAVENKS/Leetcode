dc97663ab7187892dc23a18c9e7c8f13b4d8329c
NAVEN K S <ksnaven@gmail.com>
2026-02-24 09:55:45 +0530

    Time: 2 ms (42.83%) | Memory: 43 MB (58.05%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character>stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek()=='(' && ch==')')
+            stack.pop();
+            else
+            stack.push(ch);
+        }
+        return stack.size();
+    }
+}
