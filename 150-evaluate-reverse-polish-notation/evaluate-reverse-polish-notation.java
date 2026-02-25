c598f09bd7f6d9e12c882b1fa3b3532c722742ab
NAVEN K S <ksnaven@gmail.com>
2026-02-25 10:13:38 +0530

    Time: 61 ms (5.60%) | Memory: 46.8 MB (12.14%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int evalRPN(String[] num) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<num.length;i++){
+            String ch = num[i];
+            System.out.print(ch+" ");
+            if(ch.length()==1 && !Character.isDigit(ch.charAt(0))){
+                int n1=stack.pop();
+                int n2=stack.pop();
+                char op=ch.charAt(0);
+                if(op=='+')
+                stack.push(n2+n1);
+                else if(op=='-')
+                stack.push(n2-n1);
+                else if(op=='*')
+                stack.push(n2*n1);
+                else
+                stack.push(n2/n1);
+            }
+            else{
+                stack.push(Integer.parseInt(ch));
+            }
+        }
+        return stack.pop();
+    }
+}
