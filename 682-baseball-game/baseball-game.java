829ad85d075ff2516abaa907a5634663e74b3748
NAVEN K S <ksnaven@gmail.com>
2026-02-26 09:55:46 +0530

    Time: 20 ms (5.12%) | Memory: 44.2 MB (6.80%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer>stack=new Stack<>();
+        for(String str:operations){
+            char ch=str.charAt(0);
+            if(Character.isDigit(ch) || ch=='-'){
+                stack.push(Integer.parseInt(str));
+            }
+            else if(ch=='+'){
+                int pop=stack.pop();
+                int sum=pop+stack.peek();
+                stack.push(pop);
+                stack.push(sum);
+            }
+            else if(ch=='D'){
+                int tem=stack.peek();
+                stack.push(stack.peek()*2);
+            }
+            else if(ch=='C'){
+                stack.pop();
+            }
+        }
+            int ans=0;
+            while(!stack.isEmpty()){
+                int pop=stack.pop();
+                ans+=pop;
+                System.out.print(pop+" ");
+            }
+        return ans;
+    }
+}
