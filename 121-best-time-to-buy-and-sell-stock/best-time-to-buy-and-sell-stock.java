596a3579559c44aab75f39bc7a2a76e4955ee50d
NAVEN K S <ksnaven@gmail.com>
2026-03-05 09:10:08 +0530

    Time: 2 ms (41.18%) | Memory: 89.1 MB (98.50%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,14 @@
 class Solution {
     public int maxProfit(int[] p) {
-        int n=p.length;
-        int o[]=new int [n];
-        o[n-1]=p[n-1];
-        int max=p[n-1];
-        for(int i=n-2;i>=0;i--){
+        int suf[]=new int [p.length];
+        int max=Integer.MIN_VALUE;
+        for(int i=p.length-1;i>=0;i--){
             max=Math.max(max,p[i]);
-            o[i]=max;
+            suf[i]=max;
         }
         max=0;
-        for(int i=0;i<n-1;i++){
-            max=Math.max(max,o[i]-p[i]);
+        for(int i=0;i<p.length;i++){
+            max=Math.max(max,suf[i]-p[i]);
         }
         return max;
     }
