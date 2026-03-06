07bc741755f80ade5f5a28ce0addb6058734d21f
NAVEN K S <ksnaven@gmail.com>
2026-03-06 11:25:12 +0530

    Time: 18 ms (5.10%) | Memory: 48 MB (14.47%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int candy(int[] r) {
+        int arr[]=new int[r.length];
+        Arrays.fill(arr,0);
+        for(int i=0;i<r.length;i++){
+            if((i-1)>=0 && r[i]>r[i-1]){
+                arr[i]+=arr[i-1];
+                arr[i]++;
+            }
+            else
+            arr[i]=1;
+        }
+        System.out.println(Arrays.toString(arr));
+        for(int i=r.length-1;i>=0;i--){
+            if((i+1)<r.length && r[i]>r[i+1] && arr[i]<=arr[i+1])
+            arr[i]=arr[i+1]+1;
+        }
+        System.out.print(Arrays.toString(arr));
+        int ans=0;
+        for(int i=0;i<r.length;i++)
+        ans+=arr[i];
+        return ans;
+    }
+}
