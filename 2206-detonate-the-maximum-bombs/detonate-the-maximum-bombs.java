95baf680570ff4e8425a59d65f3cf98f7478fe9b
NAVEN K S <ksnaven@gmail.com>
2026-03-09 14:20:25 +0530

    Time: 121 ms (33.38%) | Memory: 47.5 MB (6.97%) - LeetSync

---
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maximumDetonation(int[][] arr) {
+
+        int n = arr.length;
+        List<Integer>[] list = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                long dx = arr[i][0] - arr[j][0];
+                long dy = arr[i][1] - arr[j][1];
+                long r = arr[i][2];
+                if (((dx * dx) + (dy * dy)) <= (r * r)) {
+                    list[i].add(j);
+                }
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<list.length;i++)
+        System.out.println(list[i]);
+        for (int i = 0; i < arr.length; i++) {
+            Stack<Integer> stack = new Stack<>();
+            int c = 0;
+            boolean vis[] = new boolean[arr.length];
+            stack.push(i);
+            while (!stack.isEmpty()) {
+                int pop = stack.pop();
+                c++;
+                vis[pop] = true;
+                for (int k : list[pop]) {
+                    if (vis[k] == false){
+                        vis[k]=true;
+                        stack.push(k);
+                    }
+                }
+            }
+            if(c==7)System.out.println(i);
+            max = Math.max(max, c);
+        }
+        return max;
+    }
+}
