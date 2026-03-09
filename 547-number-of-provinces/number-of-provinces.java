e45cd297f369a516169db0e66ae6357dd0b7ff75
NAVEN K S <ksnaven@gmail.com>
2026-03-09 11:52:14 +0530

    Time: 5 ms (10.64%) | Memory: 49.8 MB (11.15%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,23 +1,33 @@
 class Solution {
-    boolean visited[];
-    public void dfs(int node,int adj[][]){
-        visited[node]=true;
-        for(int i=0;i<adj[node].length;i++){
-            if(!visited[i] && adj[node][i]==1 && i!=node){
-                dfs(i,adj);
-            }
-        }
-    }
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        visited=new boolean[n];
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,isConnected);
-                ans++;
+        List<Integer> list[] = new ArrayList[isConnected.length];
+        for(int i=0;i<isConnected.length;i++)
+        list[i]=new ArrayList<>();
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=0;j<isConnected[0].length;j++){
+                if(i!=j && isConnected[i][j]==1){
+                    list[i].add(j);
+                }
             }
         }
+        boolean vis[] = new boolean[isConnected.length];
+        int ans=0;
+        for(int i=0;i<isConnected.length;i++){
+            Stack<Integer>stack=new Stack<>();
+            stack.push(i);
+            if(vis[i]==false){
+                ans++;
+                while(!stack.isEmpty()){
+                    int j=stack.pop();
+                    vis[j]=true;
+                    for(int k:list[j]){
+                        if(vis[k]==false)
+                        stack.push(k);
+                        vis[k]=true;
+                    }
+                }
+            }
+        } 
         return ans;
     }
 }
