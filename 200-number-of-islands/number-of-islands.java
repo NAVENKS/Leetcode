529176f95e0dd9396c7bb52ab7d8827b1eefc61f
NAVEN K S <ksnaven@gmail.com>
2026-03-11 09:19:25 +0530

    Time: 9 ms (8.17%) | Memory: 54.9 MB (5.48%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,28 @@
 class Solution {
-    public int numIslands(char[][] g) {
-        int dir[][]={{0,1},{0,-1},{1,0},{-1,0}};
-        Stack<int []>stack=new Stack<>();
-        int u=1;
-        int m[][]=new int [g.length][g[0].length];
-        for(int i=0;i<g.length;i++){
-            for(int j=0;j<g[0].length;j++){
-                if(g[i][j]=='1'){
-                        m[i][j]=u++;
-                    stack.push(new int[]{i,j});
-                    while(!stack.isEmpty()){
-                        int n[]=stack.pop();
-                        g[n[0]][n[1]] = '0';
-                        for(int in=0;in<dir.length;in++){
-                            int wr=n[0]+dir[in][0];
-                            int wc=n[1]+dir[in][1];
-                            if(wr>=0&&wc>=0&&wr<g.length&&wc<g[0].length&&g[wr][wc]=='1'){
-                                    stack.push(new int[]{wr,wc});
-                                    m[wr][wc]=m[n[0]][n[1]]; 
+    public int numIslands(char[][] arr) {
+        int u=0;
+        int dir[][]={{1,0},{0,1},{-1,0},{0,-1}};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                Queue<int []> que = new LinkedList<>();
+                if(arr[i][j]=='1'){
+                    u++;
+                    que.offer(new int[]{i,j});
+                    arr[i][j]='0';
+                    while(!que.isEmpty()){
+                        int n[]=que.poll();
+                        for(int d[]:dir){
+                            int r=n[0]+d[0];
+                            int c=n[1]+d[1];
+                            if(r>=0 && c>=0 && r<arr.length && c<arr[0].length && arr[r][c]=='1'){
+                                que.offer(new int[]{r,c});
+                                arr[r][c]='0';
                             }
                         }
                     }
                 }
             }
         }
-        return u-1;
+        return u;
     }
 }
