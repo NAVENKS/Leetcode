c168a3e943ec545517b89539db37f22286ac805f
NAVEN K S <ksnaven@gmail.com>
2026-03-18 14:53:04 +0530

    Time: 435 ms (22.66%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select a.firstName,a.lastName,b.city,b.state
+select firstName,lastName,city,state
 from Person a
 left join Address b
 on a.personId=b.personId;
